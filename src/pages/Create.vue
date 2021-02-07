152d919ca275876e29d58b65a3fe1c57d021bc85
Julian Toscani <julian_toscani@gmx.de>
2021-02-07 01:05:01 +0100

    add boilerplate pages

---
+++ src/pages/Create.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Create!</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Create',
+});
+</script>
---
+++ src/pages/Search.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Search!</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Search',
+});
+</script>
