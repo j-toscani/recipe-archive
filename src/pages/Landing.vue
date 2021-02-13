3470d6ab30571ff15f684b1bad8dc625e40295e7
Julian Toscani <julian_toscani@gmx.de>
2021-02-13 12:24:07 +0100

    scaffold all forms

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -3,6 +3,7 @@
     <div class="landing__container">
       <h1>Please submit the Password.</h1>
       <form id="authenticate" @submit.prevent="checkPassword">
+        <input v-model="name" type="text" />
         <input v-model="password" type="text" />
         <button type="submit" form="authenticate">Enter</button>
       </form>
@@ -21,6 +22,7 @@ export default defineComponent({
   setup() {
     const feedback = useTimeoutRef('', '', 3000);
     const password = ref('');
+    const name = ref('');
     const { isAuthenticated, setIsAuthenticated } = injectUseIsAuthenticated();
 
     function checkPassword() {
@@ -36,6 +38,7 @@ export default defineComponent({
     return {
       feedback,
       password,
+      name,
       checkPassword,
       isAuthenticated,
     };
